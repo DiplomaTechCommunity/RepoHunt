f0b2cb3c725c6a2b73b7ecec28d0c6137b68b2cf
Nashrah Ansari <117299452+nashrah1307@users.noreply.github.com>
2023-11-25 19:57:56 +0530

    Benefits and DTC based creation section complete

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,52 @@
+
+const Benefits = () => {
+  return (
+    <div id="Benefits" className="benefits-section">
+      <div className="section-header">Benefits</div>
+      <div className="cards">
+        <div className="card-body">
+          <div className="card-text">
+            <p>Search Optimization</p>
+          </div>
+          <div className="card-img">
+            <img src="images/searchOptimization.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Preference-Based Experience</p>
+          </div>
+          <div className="card-img">
+            <img src="images/preferenceBased.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Prioritized Results</p>
+          </div>
+          <div className="card-img">
+            <img src="images/prioritizedResults.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Time Effefiency</p>
+          </div>
+          <div className="card-img">
+            <img src="images/timeEfficiency.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Exploration Made Easy</p>
+          </div>
+          <div className="card-img">
+            <img src="images/exploration.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Benefits;
