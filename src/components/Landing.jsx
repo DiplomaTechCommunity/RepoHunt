ee311fe1633896a2266304b2735e25d85b9ce0f7
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-11-26 19:44:25 +0530

    Integration

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -30,6 +30,57 @@ const Landing = () => {
         </div>
       </div>
     </div>
+    <div id="Benefits" className="benefits-section">
+      <div className="section-header">Benefits</div>
+      <div className="cards">
+        <div className="card-body">
+          <div className="card-text">
+            <p>Search Optimization</p>
+          </div>
+          <div className="card-img">
+            <img src="images/searchOptimization.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Preference-Based Experience</p>
+          </div>
+          <div className="card-img">
+            <img src="images/preferenceBased.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Prioritized Results</p>
+          </div>
+          <div className="card-img">
+            <img src="images/prioritizedResults.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Time Effefiency</p>
+          </div>
+          <div className="card-img">
+            <img src="images/timeEfficiency.png"/>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card-text">
+            <p>Exploration Made Easy</p>
+          </div>
+          <div className="card-img">
+            <img src="images/exploration.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="Additional-info" className="additional-section">
+      <div className="section-header">REPOHUNT IS A DTC BASED CREATION</div>
+      <div className="information">
+        <p>Diploma Tech Community is a thriving hub for coding enthusiasts among diploma students. DTC is committed to encourage learning and collaboration in the dynamic field of computer science. Our community organizes an array of events, including sessions, workshops, open source programs, hackathons, and more.</p>
+      </div>
+    </div>
     </div>
   );
 };
