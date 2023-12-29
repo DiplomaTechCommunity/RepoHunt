9ffcee54e89766d073308dcf7054b04f521721f7
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-12-29 18:48:22 +0100

    filter stylization

--- src/components/GitHubRepositories.jsx
+++ src/components/GitHubRepositories.jsx
@@ -1,177 +1,304 @@
-import { useEffect, useState, useMemo } from 'react';
-import Search from './Search';
-import Footer from "../components/Footer"
-import Cards from './Cards';
+import { useEffect, useState, useMemo } from "react";
+import Search from "./Search";
+import Footer from "../components/Footer";
+import Cards from "./Cards";
+import Select from "react-select";
 
 const GitHubRepositories = () => {
-  // Storing repositories fetched from Github API
-  const [repositories, setRepositories] = useState([]);
-  const [filterRepo, setfilterRepo] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const allLanguages = useMemo(()=>new Set(), []);
- const [filter, setfilter] = useState({
-  language: "Languages",
-   star: "Stars",
-   label: "Labels"
- })
+	// Storing repositories fetched from Github API
+	const [repositories, setRepositories] = useState([]);
+	const [filterRepo, setfilterRepo] = useState([]);
+	const [searchQuery, setSearchQuery] = useState("");
+	const allLanguages = useMemo(() => new Set(), []);
+	const [filter, setfilter] = useState({
+		language: "Languages",
+		star: "Stars",
+		label: "Labels",
+	});
 
- 
+	// eslint-disable-next-line no-undef
+	const accessToken = process.env.NEXT_PUBLIC_TOKEN;
+	// const accessToken = process.env.VITE_TOKEN // For local Add an .env file.
 
-  // eslint-disable-next-line no-undef
-  const accessToken = process.env.NEXT_PUBLIC_TOKEN;  
-  // const accessToken = process.env.VITE_TOKEN // For local Add an .env file. 
+	const repoDataFilter = async (repo) => {
+		// Fetch languages for each repository
+		const response = await fetch(
+			`https://api.github.com/repos/${repo.owner.login}/${repo.name}`,
+			{ headers: { Authorization: `Bearer ${accessToken}` } },
+		);
+		const data = await response.json();
 
-  const repoDataFilter = async (repo)=>{
-    // Fetch languages for each repository
-    const response = await fetch(`https://api.github.com/repos/${repo.owner.login}/${repo.name}`, { headers: { Authorization: `Bearer ${accessToken}` } });
-    const data = await response.json();
+		const langResponse = await fetch(repo.languages_url, {
+			headers: { Authorization: `Bearer ${accessToken}` },
+		});
+		const languagesData = await langResponse.json();
+		const languages = Object.keys(languagesData).join(", ").split(",");
+		languages.forEach((lang) => {
+			allLanguages.add(lang);
+		});
 
-    const langResponse = await fetch(repo.languages_url, { headers: { Authorization: `Bearer ${accessToken}` } });
-    const languagesData = await langResponse.json();
-    const languages = Object.keys(languagesData).join(', ').split(",");
-    languages.forEach((lang)=>{
-    allLanguages.add(lang)
-    })
-    
-    const response_2 = await fetch(`https://api.github.com/repos/${repo.owner.login}/${repo.name}/labels`, { headers: { Authorization: `Bearer ${accessToken}` } });
-    let labels = await response_2.json();
-    const labelArray = []
-    labels.filter((lab)=>{
-      labelArray.push(lab.name)
-    })
-    
-    return {
-      ...data,
-      labels: labelArray,
-    };
-  }
+		const response_2 = await fetch(
+			`https://api.github.com/repos/${repo.owner.login}/${repo.name}/labels`,
+			{ headers: { Authorization: `Bearer ${accessToken}` } },
+		);
+		let labels = await response_2.json();
+		const labelArray = [];
+		labels.filter((lab) => {
+			labelArray.push(lab.name);
+		});
 
-  useEffect(() => {
-    fetch('https://api.github.com/repositories', { headers: { Authorization: `Bearer ${accessToken}` } })
-      .then(response => response.json())
-      .then(data => {
-        // Map over the repositories and fetch additional data for each
-        const repoPromises = data.map(async repo => {
-           return await repoDataFilter(repo)
-        });
+		return {
+			...data,
+			labels: labelArray,
+		};
+	};
 
-        // Wait for all promises to resolve
-        return Promise.all(repoPromises);
-      })
-      .then(updatedRepositories => {
-        setRepositories(updatedRepositories)
-        setfilterRepo(updatedRepositories)
-      })
-      .catch(error => console.log(error));
-  }, [setRepositories, allLanguages, accessToken]);
+	useEffect(() => {
+		fetch("https://api.github.com/repositories", {
+			headers: { Authorization: `Bearer ${accessToken}` },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				// Map over the repositories and fetch additional data for each
+				const repoPromises = data.map(async (repo) => {
+					return await repoDataFilter(repo);
+				});
 
+				// Wait for all promises to resolve
+				return Promise.all(repoPromises);
+			})
+			.then((updatedRepositories) => {
+				setRepositories(updatedRepositories);
+				setfilterRepo(updatedRepositories);
+			})
+			.catch((error) => console.log(error));
+	}, [setRepositories, allLanguages, accessToken]);
 
+	// Search
+	const search = (val) => {
+		// Filter repositories based on the search query
+		const filteredRepositories = repositories.filter((repo) =>
+			repo.name.toLowerCase().includes(val.toLowerCase()),
+		);
+		// Update the state with filtered repositories
+		if (val === "") setfilterRepo(repositories);
+		setfilterRepo(filteredRepositories);
+	}; // Re-run this effect whenever the search query changes
 
-  // Search
-  const search = (val) => {
-    // Filter repositories based on the search query
-    const filteredRepositories = repositories.filter((repo) =>
-      repo.name.toLowerCase().includes(val.toLowerCase())
-    );
-    // Update the state with filtered repositories
-    if(val === '') setfilterRepo(repositories)
-    setfilterRepo(filteredRepositories);
-  }; // Re-run this effect whenever the search query changes
+	const handleFilter = async (selectedOption, actionMeta) => {
+		const name = actionMeta.name;
+		const value = selectedOption ? selectedOption.value : "";
 
-  const handleFilter = async (e)=>{
-    let {name, value} = e.target
-    setfilter((prev)=>{
-     return {...prev, [name]:value}
-    })
+		setfilter((prev) => ({ ...prev, [name]: value }));
 
-    let temp = {
-      ...filter,
-      [name]:value
-    }
+		let temp = {
+			...filter,
+			[name]: value,
+		};
 
-    let query = ""
+		let query = "";
 
-    if(temp.language !== "Languages"){
-      query += `language:${temp.language}`
-    }
+		if (temp.language !== "Languages") {
+			query += `language:${temp.language}`;
+		}
 
-    if(temp.star !== "Stars"){
-      query +=`+stars:${temp.star}`
-    }
-   
-    await fetch(`https://api.github.com/search/repositories?q=${query}`)
-    .then((response)=>response.json())
-    .then(data => {
-      const repoPromises = data.items.map(async repo => {
-         return await repoDataFilter(repo)
-      });
+		if (temp.star !== "Stars") {
+			query += `+stars:${temp.star}`;
+		}
 
-      return Promise.all(repoPromises);
-    })
-    .then(updatedRepositories => {
-      if(temp.label !== "Labels"){
-        updatedRepositories = updatedRepositories.filter(async (repo)=>{
-          return await repo.labels.includes(temp.label)
-        })
-      }
-      setfilterRepo(updatedRepositories)
-    })
-    .catch(error => console.log(error));
+		await fetch(`https://api.github.com/search/repositories?q=${query}`)
+			.then((response) => response.json())
+			.then((data) => {
+				const repoPromises = data.items.map(async (repo) => {
+					return await repoDataFilter(repo);
+				});
 
-  }
+				return Promise.all(repoPromises);
+			})
+			.then((updatedRepositories) => {
+				if (temp.label !== "Labels") {
+					updatedRepositories = updatedRepositories.filter(async (repo) => {
+						return await repo.labels.includes(temp.label);
+					});
+				}
+				setfilterRepo(updatedRepositories);
+			})
+			.catch((error) => console.log(error));
+	};
 
-  return (
-   <div className='bg-[#0B0E1A] '>
-    <div className='min-h-screen'>
-      <Search onSearch={search} searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-       <div className='text-center m-12 text-white text-3xl'>Filter your search using Languages, Labels and Stars</div>
+	const optionsLabel = [
+		{ value: "Labels", label: "Labels" },
+		{ value: "bug", label: "bug" },
+		{ value: "feature", label: "feature" },
+		{ value: "enhancement", label: "enhancement" },
+		{ value: "documentation", label: "documentation" },
+		{ value: "good first issue", label: "good first issue" },
+		{ value: "help wanted", label: "help wanted" },
+		{ value: "critical", label: "critical" },
+		{ value: "algorithm", label: "algorithm" },
+	];
 
-<div className='flex m-12 justify-around'>
-<select id="language" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="language" value={filter.language} onChange={handleFilter}>
-  <option defaultValue={"Languages"}>Languages</option>
-  {
-    Array.from(allLanguages).map((lang, index)=>{
-      if(lang !== "") {
-      return <option key={index} value={lang}>{lang}</option>
-      }
-    })
-  }
-</select>
+	const optionsStar = [
+		{ value: "Stars", label: "Stars" },
+		{ value: "0..50", label: "<50" },
+		{ value: "50..100", label: "<100" },
+		{ value: "100..150", label: "<150" },
+		{ value: "150..200", label: "<200" },
+		{ value: "200..500", label: "<500" },
+		{ value: ">500", label: ">500" },
+	];
 
-<select id="label" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name='label' value={filter.label} onChange={handleFilter}>
-  <option defaultChecked={"Labels"}>Labels</option>
-  <option value="bug">bug</option>
-  <option value="feature">feature</option>
-  <option value="enhancement">enhancement</option>
-  <option value="documentation">documentation</option>
-  <option value="good first issue">good first issue</option>
-  <option value="help wanted">help wanted</option>
-  <option value="critical">critical</option>
-  <option value="algorithm">algorithm</option>
-</select>
-<select id="star" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name='star' value={filter.star} onChange={handleFilter}>
-  <option defaultChecked="Stars">Stars</option>
-  <option value="0..50">{'<50'}</option>
-  <option value="50..100">{'<100'}</option>
-  <option value="100..150">{'<150'}</option>
-  <option value="150..200">{'<200'}</option>
-  <option value="200..500">{'<500'}</option>
-  <option value=">500">{'>500'}</option>
-</select>
-      </div>
+	const optionsLanguage = [
+		{ value: "Languages", label: "Languages" },
+		...Array.from(allLanguages)
+			.filter((lang) => lang !== "")
+			.map((lang) => ({
+				value: lang,
+				label: lang,
+			})),
+	];
 
-      <div>
-        <div className='grid grid-cols-4 justify-around p-4 items-stretch'>
-          {filterRepo.map((repo, index) => (
-            <Cards repo={repo} key={index}/>
-          ))}
-        </div>
-      </div>
-    </div>
-      <Footer/>
- </div>
-  );
+	const customStyles = {
+		control: (styles) => ({
+			...styles,
+			padding: "5px 10px",
+			borderRadius: "0.5rem",
+			borderWidth: "1px",
+			borderColor: "#D1D5DB",
+			fontSize: "0.875rem",
+			lineHeight: "1.25rem",
+			color: "#111827",
+			backgroundColor: "#F9FAFB",
+			cursor: "pointer",
+			minWidth: "150px",
+			":hover": {
+				backgroundColor: "#a1a1a1",
+				color: "black",
+			},
+		}),
+		dropdownIndicator: (styles) => ({
+			...styles,
+			color: "black",
+			"&:hover": {
+				...styles[`:hover`],
+				color: "white",
+			},
+		}),
+		option: (styles, { isFocused, isSelected }) => {
+			return {
+				...styles,
+				fontSize: "0.875rem",
+				lineHeight: "1.25rem",
+				cursor: "pointer",
+				color: "#111827",
+				backgroundColor: isFocused
+					? "#777c8e"
+					: isSelected
+					? "darkgray"
+					: "#F9FAFB",
+				":hover": {
+					backgroundColor: "#777c8e",
+					color: "black",
+				},
+			};
+		},
+		menu: (styles) => ({
+			...styles,
+			menuShouldBlockScroll: true,
+		}),
+		menuList: (styles) => ({
+			...styles,
+			menuShouldScrollIntoView: true,
+			overflowY: "auto",
+			maxHeight: "250px",
+			"&::-webkit-scrollbar": {
+				width: "6px",
+				height: "6px",
+			},
+			"&::-webkit-scrollbar-track": {
+				borderRadius: "10px",
+				background: "rgba(0, 0, 0, 0.1)",
+			},
+			"&::-webkit-scrollbar-thumb": {
+				borderRadius: "10px",
+				backgroundColor: "rgba(0, 0, 0, 0.2)",
+			},
+			"&::-webkit-scrollbar-thumb:hover": {
+				backgroundColor: "rgba(0, 0, 0, 0.4)",
+			},
+			"&::-webkit-scrollbar-thumb:active": {
+				backgroundColor: "rgba(0, 0, 0, 0.5)",
+			},
+		}),
+		singleValue: (styles) => ({
+			...styles,
+			fontSize: "0.875rem",
+			lineHeight: "1.25rem",
+			color: "#111827",
+		}),
+	};
+
+	return (
+		<div className="bg-[#0B0E1A] ">
+			<div className="min-h-screen">
+				<Search
+					onSearch={search}
+					searchQuery={searchQuery}
+					setSearchQuery={setSearchQuery}
+				/>
+				<div className="text-center m-12 text-white text-3xl">
+					Filter your search using Languages, Labels and Stars
+				</div>
+
+				<div className="flex m-12 justify-around">
+					<Select
+						id="language"
+						name="language"
+						options={optionsLanguage}
+						styles={customStyles}
+						defaultValue={optionsLanguage[0]}
+						value={optionsLanguage.find(
+							(option) => option.value === filter.language,
+						)}
+						onChange={(selectedOption, actionMeta) =>
+							handleFilter(selectedOption, actionMeta)
+						}
+					/>
+					<Select
+						id="label"
+						name="label"
+						options={optionsLabel}
+						styles={customStyles}
+						defaultValue={optionsLabel[0]}
+						value={optionsLabel.find((option) => option.value === filter.label)}
+						onChange={(selectedOption, actionMeta) =>
+							handleFilter(selectedOption, actionMeta)
+						}
+					/>
+					<Select
+						id="star"
+						name="star"
+						options={optionsStar}
+						styles={customStyles}
+						defaultValue={optionsStar[0]}
+						value={optionsStar.find((option) => option.value === filter.star)}
+						onChange={(selectedOption, actionMeta) =>
+							handleFilter(selectedOption, actionMeta)
+						}
+					/>
+				</div>
+
+				<div>
+					<div className="grid grid-cols-4 justify-around p-4 items-stretch">
+						{filterRepo.map((repo, index) => (
+							<Cards repo={repo} key={index} />
+						))}
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
-
 
 export default GitHubRepositories;
